8f9fc8b341b1172ef305ba91c70dbffd52112a4a
Mattia <162014917+0tia0@users.noreply.github.com>
2026-01-22 20:53:08 +0100

    Project created

---
+++ src/main/java/me/mattia/maze/InfiniteMaze.java
@@ -0,0 +1,17 @@
+package me.mattia.maze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InfiniteMaze extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
